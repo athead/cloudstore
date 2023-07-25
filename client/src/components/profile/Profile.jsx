e4f6b528de572cbd410806c37de774c1652c3fdf
vskill <vskill@mail.ru>
2023-07-25 19:31:49 +0300

    deploy error fixed

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./profile.css";
 import { useDispatch, useSelector } from "react-redux";
 import { authAction, changePasswordAction } from "../../actions/user";
